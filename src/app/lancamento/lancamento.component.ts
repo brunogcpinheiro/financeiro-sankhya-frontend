916e276455615c622affa7d0a58c2f2aa7bb9935
Bruno Pinheiro <brunogcpinheiro@gmail.com>
2019-05-12 21:43:03 -0300

    listando lancamentos do banco de dados

--- src/app/lancamento/lancamento.component.ts
+++ src/app/lancamento/lancamento.component.ts
@@ -1,9 +1,28 @@
 import { Component, OnInit } from "@angular/core";
-import { Lancamento } from "./lancamento";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
 	selector: "app-lancamento",
 	templateUrl: "./lancamento.component.html",
 	styleUrls: [ "./lancamento.component.css" ],
 })
-export class LancamentoComponent {}
+export class LancamentoComponent implements OnInit {
+	response: any;
+	readonly API_URL = "http://localhost:8080/financeiro/api";
+	constructor (private _http: HttpClient) {}
+
+	ngOnInit (): void {
+		this.listarLancamentos();
+	}
+
+	listarLancamentos () {
+		return this._http.get(`${this.API_URL}/lancamentos`).subscribe(
+			data => {
+				this.response = data;
+			},
+			error => {
+				console.log("Aconteceu um erro: ", error);
+			},
+		);
+	}
+}
