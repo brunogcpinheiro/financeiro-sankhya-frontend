a54b6a2c6c6fa8e2992fba2ef397ffb7cc0b143d
Bruno Pinheiro <brunogcpinheiro@hotmail.com>
2019-05-20 20:04:45 +0000

    adicionando datepicker no formulario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,8 @@ import { HttpClientModule } from "@angular/common/http";
 import { ReactiveFormsModule } from "@angular/forms";
 import { ChartsModule } from "ng2-charts";
 import { registerLocaleData } from "@angular/common";
-import { CurrencyMaskModule } from "ng2-currency-mask"
+import { CurrencyMaskModule } from "ng2-currency-mask";
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 import pt from "@angular/common/locales/pt";
 registerLocaleData(pt, "pt");
 
@@ -14,6 +15,7 @@ import { LancamentoComponent } from "./lancamento/lancamento.component";
 import { NovoLancamentoComponent } from "./lancamento/novo-lancamento.component";
 import { LancamentoService } from "./lancamento/lancamento.service";
 import { ChartComponent } from "./chart/chart.component";
+
 
 @NgModule({
 	declarations: [
@@ -28,7 +30,8 @@ import { ChartComponent } from "./chart/chart.component";
 		HttpClientModule,
 		ReactiveFormsModule,
 		ChartsModule,
-		CurrencyMaskModule
+		CurrencyMaskModule,
+		BsDatepickerModule.forRoot()
 	],
 	providers: [
 		LancamentoService,
